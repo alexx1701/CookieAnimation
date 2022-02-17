e7033aa44cb6e710d077838c5b813b054dc43205
alexx1701 <92756383+alexx1701@users.noreply.github.com>
2022-02-17 13:50:03 -0600

    fixed overflow and button color

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ tl.fromTo('.cookie-container', {scale:0}, {scale:1, ease: "elastic.out(1, 0.75)"
 tl.fromTo('.cookie', {opacity:0, x:-50, rotation: '-45deg'}, {opacity:1, x:0, rotation: '0deg'})
 tl.fromTo('.text', {x:30, opacity:0}, {x:0, opacity:1}, '<')
 
-tl.fromTo('.cookie', {y:0, rotation: '0deg'}, {y: -20, yoyo: true, repeat: -1, rotation: '-20deg'})
-tl.fromTo('#crumbs', {y:0}, {y: -20, yoyo: true, repeat: -1}, '<')
+tl.fromTo('.cookie', {y:0, rotation: '0deg'}, {y: -15, yoyo: true, repeat: -1, rotation: '-20deg'})
+tl.fromTo('#crumbs', {y:0}, {y: -5, yoyo: true, repeat: -1}, '<')
 
 
 //fade out button
